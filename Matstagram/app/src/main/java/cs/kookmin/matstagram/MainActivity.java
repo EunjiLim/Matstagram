6ec1b41ce3d81f2708ddc00fe4a0cda9def51dea
EunjiLim <cucumbers816@gmail.com>
2016-01-10 02:09:21 +0900

    add Instagram API library

--- Matstagram/app/src/main/java/cs/kookmin/matstagram/MainActivity.java
+++ Matstagram/app/src/main/java/cs/kookmin/matstagram/MainActivity.java
@@ -1,38 +1,96 @@
 package cs.kookmin.matstagram;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import br.com.dina.oauth.instagram.InstagramApp;
+import br.com.dina.oauth.instagram.InstagramApp.OAuthAuthenticationListener;
+import cs.kookmin.matstagram.instagram.InstagramData;
+
+import static cs.kookmin.matstagram.instagram.InstagramData.*;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    private InstagramApp mApp;
+    private Button btnConnect;
+    private TextView tvSummary;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        mApp = new InstagramApp(this, CLIENT_ID,
+                CLIENT_SECRET, CALLBACK_URL);
+        mApp.setListener(listener);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        tvSummary = (TextView) findViewById(R.id.tvSummary);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        btnConnect = (Button) findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (mApp.hasAccessToken()) {
+                    final AlertDialog.Builder builder = new AlertDialog.Builder(
+                            MainActivity.this);
+                    builder.setMessage("Disconnect from Instagram?")
+                            .setCancelable(false)
+                            .setPositiveButton("Yes",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(
+                                                DialogInterface dialog, int id) {
+                                            mApp.resetAccessToken();
+                                            btnConnect.setText("Connect");
+                                            tvSummary.setText("Not connected");
+                                        }
+                                    })
+                            .setNegativeButton("No",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(
+                                                DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    });
+                    final AlertDialog alert = builder.create();
+                    alert.show();
+                } else {
+                    mApp.authorize();
+                }
+            }
+        });
+
+        if (mApp.hasAccessToken()) {
+            tvSummary.setText("Connected as " + mApp.getUserName());
+            btnConnect.setText("Disconnect");
         }
 
-        return super.onOptionsItemSelected(item);
     }
+
+    OAuthAuthenticationListener listener = new OAuthAuthenticationListener() {
+
+        @Override
+        public void onSuccess() {
+            tvSummary.setText("Connected as " + mApp.getUserName());
+            btnConnect.setText("Disconnect");
+        }
+
+        @Override
+        public void onFail(String error) {
+            Log.i("InstagramAPI", "onFail");
+            Toast.makeText(MainActivity.this, error, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
---
+++ Matstagram/app/src/main/java/cs/kookmin/matstagram/instagram/InstagramData.java
@@ -0,0 +1,10 @@
+package cs.kookmin.matstagram.instagram;
+
+/**
+ * Created by Eunji on 2016-01-09.
+ */
+public class InstagramData {
+    public static final String CLIENT_ID = "f61b0f53e1b743798ed8d05b7dbc49d9";
+    public static final String CLIENT_SECRET = "d699d1095acc434f941975ff1af0188d";
+    public static final String CALLBACK_URL = "http://blog.naver.com/i_peak";
+}
